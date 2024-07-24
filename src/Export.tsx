8f444dfbbd43d41adc1e8cd572a99f9430be98c7
wifv <abcogenshin@gmail.com>
2024-07-24 20:20:05 +0500

    v1

--- src/Export.tsx
+++ src/Export.tsx
@@ -1,1 +1,1 @@
-export const API = import.meta.env.VITE_API_URL || 'http://192.168.1.18:8080/api/v1/';
+export const API = import.meta.env.VITE_API_URL || 'https://e568-90-156-162-42.ngrok-free.app/api/v1/';
