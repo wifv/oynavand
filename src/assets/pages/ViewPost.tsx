3af87814a3707774cab1f7b5aae30f44924b61f2
wifv <abcogenshin@gmail.com>
2024-07-24 18:06:05 +0500

    v1

--- src/assets/pages/ViewPost.tsx
+++ src/assets/pages/ViewPost.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { API } from '../../Export'
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
